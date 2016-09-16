0fed28f91ac15395df7f0ea47a3c628f4920d3d4
John Hamm <johndhamm1@gmail.com>
2016-09-16 13:47:20 -0500

    getting deployment working 2

--- server.js
+++ server.js
@@ -41,24 +41,37 @@ app.use((req, res) =>
 )
 
 // Error handling middleware
-app.use((
-    err,
-    { method, url, headers: { 'user-agent': agent } },
-    res,
-    next
-  ) => {
-    res.sendStatus(err.status || 500)
+app.use((err, { method, url, headers: { 'user-agent': agent } }, res, next) => {
+  res.sendStatus(err.status || 500)
 
-    const timeStamp = new Date()
-    const statusCode = res.statusCode
-    const statusMessage = res.statusMessage
+  const timeStamp = new Date()
+  const statusCode = res.statusCode
+  const statusMessage = res.statusMessage
 
-    console.error(
-      `[${timeStamp}] "${chalk.red(`${method} ${url}`)}" Error (${statusCode}): "${statusMessage}"`
-    )
-    console.error(err.stack)
-  }
-)
+  console.error(
+    `[${timeStamp}] "${chalk.red(`${method} ${url}`)}" Error (${statusCode}): "${statusMessage}"`
+  )
+  console.error(err.stack)
+})
+
+// app.use((
+//     err,
+//     { method, url, headers: { 'user-agent': agent } },
+//     res,
+//     next
+//   ) => {
+//     res.sendStatus(err.status || 500)
+
+//     const timeStamp = new Date()
+//     const statusCode = res.statusCode
+//     const statusMessage = res.statusMessage
+
+//     console.error(
+//       `[${timeStamp}] "${chalk.red(`${method} ${url}`)}" Error (${statusCode}): "${statusMessage}"`
+//     )
+//     console.error(err.stack)
+//   }
+// )
 
 // Listen to requests on the provided port and log when available
 
