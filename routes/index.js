f8df2b7a22c2dafccbdcfe4de4d920a83e195518
Alex Craven <w.alex.craven@gmail.com>
2016-09-16 15:55:46 -0500

    implemented up/downvotes

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,19 @@ router.get('/', (req, res, err) => {
 	// res.render('index')
 })
 
+router.post('/', (req, res, err) => {
+	let id = req.body.upvote || req.body.downvote
+	let scoreChanger
+	(Object.keys(req.body)[0] === 'upvote') ? scoreChanger = 1 : scoreChanger = -1
+	Post
+		.update({_id: id}, {$inc: {score: scoreChanger}})
+		.then(() => res.redirect('/'))
+		.catch(err)
+})
+
 router.get('/post', (req, res) => {
 	res.render('post', {page: 'Post'})
 })
-
 
 router.post('/post', (req, res, err) =>
 	Post
