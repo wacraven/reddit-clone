41bba00294011c7e8c150cbe477d1e590353a2a4
John Hamm <johndhamm1@gmail.com>
2016-09-16 13:37:50 -0500

    getting deployment working

--- database.js
+++ database.js
@@ -2,8 +2,8 @@
 
 const mongoose = require('mongoose')
 // const MONGODB_URL = 'mongodb://localhost:27017/freddit'
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/freddit'
-// const MONGODB_URL = 'mongodb://fredditMaster:12345678@ds033106.mlab.com:33106/freddit'
+// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/freddit'
+const MONGODB_URL = 'mongodb://fredditMaster:12345678@ds033106.mlab.com:33106/freddit'
 
 
 mongoose.Promise = Promise //tells mongoose to use global Promise vs. its' internal promise
